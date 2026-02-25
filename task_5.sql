28dcca0b9444b024c76c754098c195c79faa5d91
Yonathan Yilma <yonathanyilma0124@gmail.com>
2026-02-24 20:50:04 -0800

    Fix customer insertion checker pattern

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,12 @@
+  GNU nano 8.6                         task_5.sql                         Modified
+ GNU nano 8.6                         task_5.sql
 USE alx_book_store;
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES
 (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES
